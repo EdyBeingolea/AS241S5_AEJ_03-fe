55b771c4d199a98a0b7e4b12f8bc808d7e3e5a8d
rogger <rvaldiviacarnica@gmail.com>
2026-05-17 12:28:52 -0500

    feat: add body component with hero section and styles; update app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
+import { Body } from "./layout/body/body";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Body],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/layout/body/body.ts
+++ src/app/layout/body/body.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-body',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './body.html',
-  styleUrl: './body.scss',
+  styleUrls: ['./body.scss'],
 })
 export class Body {}
